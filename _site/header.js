7be121e4d4f3b4ccdbf0f22fbe2e6ab317a2cd5b
CE-sw <2seowon@naver.com>
2022-06-19 22:28:12 +0900

    .

--- _site/header.js
+++ _site/header.js
@@ -1,9 +1,10 @@
 var header = `
 <div class="wrap">
-<div class="logo">
+<div class="hvr-float-shadow logo">
     <a href="index.html"><img src="./images/simplelogo.png"></a>
 </div>
 <div class="line">
+    <button class = "hvr-float-shadow menu" ><a href="#">Apply</a></button>
     <button class = "hvr-float-shadow menu" ><a href="nav.html">&#9776;</a></button>
 </div>
 </div>
