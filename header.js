3631d3f424e58180f9735b6df74c5660a12e9b21
CE-sw <2seowon@naver.com>
2022-06-19 21:26:54 +0900

    .

---
+++ header.js
@@ -0,0 +1,12 @@
+var header = `
+<div class="wrap">
+<div class="logo">
+    <a href="index.html"><img src="./images/simplelogo.png"></a>
+</div>
+<div class="line">
+    <button class = "hvr-float-shadow menu" ><a href="nav.html">&#9776;</a></button>
+</div>
+</div>
+`;
+
+document.write(header)
